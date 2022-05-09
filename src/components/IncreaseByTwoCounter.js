4d84c11bd9a74d3c635988f023c3f20c2ef0a42c
hfzehra <hfzehra.uysal33@gmail.com>
2022-05-09 11:46:18 +0300

    redux-intro-project completed

---
+++ src/components/IncreaseByTwoCounter.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { increaseByTwoCounter } from '../redux/actions/counterActions'
+
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+class IncreaseByTwoCounter extends Component {
+  render() {
+    return (
+      <div>
+        <button onClick={e=>{
+          this.props.dispatch(increaseByTwoCounter())
+        }}> 2 arttır
+        </button>
+      </div>
+    )
+  }
+}
+
+function mapDispatchProps(dispatch){
+  return{action:bindActionCreators(increaseByTwoCounter,dispatch)}
+}
+
+export default connect(mapDispatchProps)(IncreaseByTwoCounter)
